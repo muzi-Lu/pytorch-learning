371dc261a175f4014a9abe91cf033aebece564ec
muzi-Lu <Arthasldb@outlook.com>
2023-04-15 23:16:25 +0800

    start HW4

---
+++ HW4/Utils.py
@@ -0,0 +1,15 @@
+import torch
+import numpy as np
+import torch.nn as nn
+import pandas as pd
+import random
+
+def set_seed(seed):
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+    # torch.backends.cudnn.benchmark = False
+    # torch.backends.cudnn.deterministic = True
