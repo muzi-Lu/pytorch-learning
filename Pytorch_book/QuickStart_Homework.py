1da1e65d63e9c8d5bdbb6d993012cf1ba86810c3
muzi-Lu <Arthasldb@outlook.com>
2023-06-09 22:55:00 +0800

    Nothing

--- Pytorch_book/QuickStart_Homework.py
+++ Pytorch_book/QuickStart_Homework.py
@@ -1,3 +1,67 @@
 '''
 Small Try：CIFAR-10
 '''
+import torchvision as tv
+import torchvision.transforms as transforms
+from torchvision.transforms import ToPILImage
+import torch
+from torch.utils.data import DataLoader, Dataset
+
+# 分类数据
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+         'dog', 'frog', 'horse', 'ship', 'truck')
+
+# 定义数据集
+
+
+show = ToPILImage()  # 可以把Tensor转换成Image, 方便可视化
+
+
+def main():
+    # 第一次运行程序torchvision会自动下载CIFAR-10数据集
+    # 大约100M，需要一定时间
+    # 如果已经下载有CIFAR-10，可通过root参数指定
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    ])
+
+    # TrainSet
+    trainset = tv.datasets.CIFAR10(
+        root='/media/benben/0ECABB60A248B50C',
+        train=True,
+        download=False,
+        transform=transform
+    )
+
+    trainloader = DataLoader(
+        trainset,
+        batch_size=4,
+        shuffle=True,
+        num_workers=2
+    )
+
+    testset = tv.datasets.CIFAR10(
+        root='/media/benben/0ECABB60A248B50C',
+        train=True,
+        download=False,
+        transform=transform
+    )
+
+    testdataloader = DataLoader(
+        trainset,
+        batch_size=4,
+        shuffle=False,
+        num_workers=2
+    )
+
+    # 试验一下
+    (data, label) = trainset[100]
+    print(classes[label])
+    show((data + 1) / 2).resize((100, 100))
+
+
+if __name__ == '__main__':
+    main()
--- Pytorch_book/torch_3_TensorAndAutograd.py
+++ Pytorch_book/torch_3_TensorAndAutograd.py
@@ -2,3 +2,16 @@
 3.1 Tensor
 
 '''
+
+'''
+3.1.1 基础操作
+tensor的接口和Numpy相似，以方便用户使用。但不熟悉Numpy也先没有关系
+
+从torch接口的角度进行分类可以分为两类：
+1.torch.function
+2.tensor.function
+
+从存储的角度来讲，tensor的操作可以分为两类：
+1.不会修改自身数据的
+2.会修改自身数据的
+'''
